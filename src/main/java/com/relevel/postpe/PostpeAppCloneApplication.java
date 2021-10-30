881f674f975f00a1f400dd05e34bb2846eb255c3
PranayKumarGoru <goru.pranay@gmail.com>
2021-10-30 22:02:11 +0530

    Initial Commit

---
+++ src/main/java/com/relevel/postpe/PostpeAppCloneApplication.java
@@ -0,0 +1,13 @@
+package com.relevel.postpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostpeAppCloneApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PostpeAppCloneApplication.class, args);
+	}
+}
+
---
+++ src/test/java/com/relevel/postpe/PostpeAppCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relevel.postpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostpeAppCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
